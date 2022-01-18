76fef7d4f2380e357204165d0fabae24e7e6b08e
bhattishani <bhattishan090@gmail.com>
2022-01-18 20:43:47 +0530

    database

--- profinxt.sql
+++ profinxt.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2022 at 03:01 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Generation Time: Jan 18, 2022 at 04:13 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,114 @@ SET time_zone = "+00:00";
 --
 -- Database: `profinxt`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `number` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `communication_with` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `wp_number` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profession` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_by` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `updated_by` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `number`, `communication_with`, `wp_number`, `profession`, `status`, `created_by`, `updated_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'sunny bhatti', '123456789', 'admin', '7874637972', 'Professional', 1, 'Admin', NULL, '2022-01-13 09:54:18', '2022-01-13 09:54:18', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_demat`
+--
+
+CREATE TABLE `client_demat` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `st_sg` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `serial_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `service_type` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pan_number` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `holder_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `broker` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mpin` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `capital` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_by` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `client_demat`
+--
+
+INSERT INTO `client_demat` (`id`, `client_id`, `st_sg`, `serial_number`, `service_type`, `pan_number`, `holder_name`, `broker`, `user_id`, `password`, `mpin`, `capital`, `created_at`, `updated_at`, `updated_by`, `deleted_at`) VALUES
+(1, 1, 'SG', '12345Abcd', '2', 'pan number', 'sunny bhatti', 'Govt Job', 'sunny', 'admin', '12345', '55000', '2022-01-13 09:54:18', '2022-01-13 09:54:18', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_mode`
+--
+
+CREATE TABLE `client_mode` (
+  `id` int(11) NOT NULL,
+  `mode` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_by` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `client_mode`
+--
+
+INSERT INTO `client_mode` (`id`, `mode`, `created_at`, `deleted_at`, `updated_at`, `updated_by`) VALUES
+(1, 'project', '2022-01-13 15:22:59', '2022-01-13 10:52:38', '2022-01-13 15:22:59', 'admin'),
+(2, 'target', '2022-01-13 15:22:59', '2022-01-13 10:52:38', '2022-01-13 15:22:59', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_payment`
+--
+
+CREATE TABLE `client_payment` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `joining_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `fees` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mode` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL,
+  `updated_by` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `client_payment`
+--
+
+INSERT INTO `client_payment` (`id`, `client_id`, `joining_date`, `fees`, `mode`, `created_at`, `updated_at`, `deleted_at`, `updated_by`) VALUES
+(1, 1, '2022-01-07 00:00:00', '55000', 1, '2022-01-13 09:54:18', '2022-01-13 09:54:18', NULL, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -110,6 +218,20 @@ CREATE TABLE `permissions` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'client-read', 'web', NULL, NULL),
+(2, 'client-write', 'web', NULL, NULL),
+(3, 'client-create', 'web', NULL, NULL),
+(4, 'client-delete', 'web', NULL, NULL),
+(5, 'role-read', 'web', NULL, NULL),
+(6, 'role-write', 'web', NULL, NULL),
+(7, 'role-create', 'web', NULL, NULL),
+(8, 'role-delete', 'web', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -147,7 +269,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'web', NULL, NULL);
+(2, 'writer', 'web', '2022-01-13 07:39:16', '2022-01-13 07:39:16'),
+(3, 'staff', 'web', '2022-01-13 07:41:16', '2022-01-13 07:41:16'),
+(4, 'user', 'web', '2022-01-18 09:25:04', '2022-01-18 09:25:04');
 
 -- --------------------------------------------------------
 
@@ -159,6 +283,15 @@ CREATE TABLE `role_has_permissions` (
   `permission_id` bigint(20) UNSIGNED NOT NULL,
   `role_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `role_has_permissions`
+--
+
+INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
+(1, 4),
+(2, 4),
+(5, 4);
 
 -- --------------------------------------------------------
 
@@ -190,6 +323,33 @@ INSERT INTO `users` (`id`, `name`, `image`, `number`, `email`, `email_verified_a
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `client_demat`
+--
+ALTER TABLE `client_demat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `client_mode`
+--
+ALTER TABLE `client_mode`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `client_payment`
+--
+ALTER TABLE `client_payment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `mode` (`mode`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -265,6 +425,30 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `client_demat`
+--
+ALTER TABLE `client_demat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `client_mode`
+--
+ALTER TABLE `client_mode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `client_payment`
+--
+ALTER TABLE `client_payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -280,7 +464,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -292,7 +476,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -303,6 +487,19 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `client_demat`
+--
+ALTER TABLE `client_demat`
+  ADD CONSTRAINT `client_demat_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+--
+-- Constraints for table `client_payment`
+--
+ALTER TABLE `client_payment`
+  ADD CONSTRAINT `client_payment_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  ADD CONSTRAINT `client_payment_ibfk_2` FOREIGN KEY (`mode`) REFERENCES `client_mode` (`id`);
 
 --
 -- Constraints for table `model_has_permissions`
