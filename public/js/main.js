a8cc428263c327441818e044e979ae5f186dd78b
shanitjcg <shanibhatti@tjcg.in>
2022-03-31 19:32:07 +0530

    dropdown css issue

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,9 @@ const disableLoader = (elem=".loading")=>{
 }
 $(document).on("show.bs.modal",".modal",function(){
     enableLoader();
-    $(".select2-container").css({"display":"none"});
 })
 $(document).on("shown.bs.modal",".modal",function(){
     disableLoader();
-})
-$(document).on("hidden.bs.modal",".modal",function(){
-    $(".select2-container").css({"display":"inline-block"});
 })
 $.ajaxSetup({
     headers: {
